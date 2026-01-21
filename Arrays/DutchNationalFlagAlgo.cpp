bf668bb582388b75473f86e6db834e22ebb17164
Fatima Nisar <fatimanisar477@gmail.com>
2026-01-21 12:42:15 +0500

    DutchFlagAlgo

---
+++ Arrays/DutchNationalFlagAlgo.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution{
+    public:
+    void sortzeroonetwo(vector<int> &arr )
+    {
+        int low=0;
+        int mid=0;
+        int high= arr.size()-1;
+        while(mid <= high)
+        {
+           if(arr[mid]==0)
+           {
+              swap(arr[low], arr[mid]);
+              low++;
+              mid++;
+           }
+           else if(arr[mid]==1){
+             mid++;
+
+           }
+           else if(arr[mid]==2)
+           { 
+               swap(arr[mid],arr[high]);
+               high--;
+
+           }
+        }
+    }
+};

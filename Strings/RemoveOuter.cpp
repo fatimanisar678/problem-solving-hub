3c3677e5f8bfba5986a653f6f76a68f66f3eb0c0
Fatima Nisar <fatimanisar477@gmail.com>
2026-01-29 11:27:04 +0500

    Strings

---
+++ Strings/RemoveOuter.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution{
+    public:
+       string removeOuterParentheses(string s){
+        string result=" ";
+        int level=0;
+        for(char ch:s)
+        {
+            if(ch=='(')
+            {
+                if(level>0)
+                {
+                    result+=ch;
+                }
+                level++;
+            }
+            else if(ch==')')
+            {
+                level--;
+                if(level>0)
+                {
+                    result+=ch;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+       
